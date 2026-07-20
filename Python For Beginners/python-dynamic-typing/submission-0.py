d1c915e584c136bb95f8fc197b4a67a3baabf348
Mehardeep Singh <mehardeepsingh0725@gmail.com>
2026-07-19 18:04:51 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 6.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Word"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
